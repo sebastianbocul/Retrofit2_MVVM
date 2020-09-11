1cd7705109f7ca68fda5ec386d8da5bcda577b93
sebastianbocul <seba23495@gmail.com>
2020-09-11 21:39:52 +0200

    Created Recipe view

---
+++ app/src/main/java/com/sebix/retrofit_mvvm/RecipeActivity.java
@@ -0,0 +1,37 @@
+package com.sebix.retrofit_mvvm;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.sebix.retrofit_mvvm.models.Recipe;
+
+class RecipeActivity extends BaseActivity {
+    private AppCompatImageView mRecipeImage;
+    private TextView mRecipeTitle,mRecipeRank;
+    private LinearLayout mRecipeIngredientsContainer;
+    private ScrollView mScrollView;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        mRecipeImage = findViewById(R.id.recipe_image);
+        mRecipeTitle = findViewById(R.id.recipe_title);
+        mRecipeRank = findViewById(R.id.recipe_social_score);
+        mRecipeIngredientsContainer = findViewById(R.id.ingredients_container);
+        mScrollView = findViewById(R.id.parent);
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+        if(getIntent().hasExtra("recipe")){
+            Recipe recipe = getIntent().getParcelableExtra("recipe");
+        }
+    }
+}
